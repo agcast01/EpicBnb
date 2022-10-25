281f6e4a58d96daf747cedd070078d01295c64b7
Alex Castillo <agcast01@gmail.com>
2022-10-24 21:02:31 -0400

    migrations added

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -52,7 +52,8 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Spot, {foreignKey: 'ownerId'});
-      User.hasMany(models.Review, {foreignKey: 'userId'})
+      User.hasMany(models.Review, {foreignKey: 'userId'});
+      User.hasMany(models.Booking, {foreignKey: 'userId'})
     }
   }
   User.init({
